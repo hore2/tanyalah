a955eae57ff23263901e5613ce98db3fda4acacf
Hayu <greenianda@gmail.com>
2020-08-14 00:49:22 +0700

    modul pertanyaan - tahap 1

---
+++ app/Http/Controllers/PertanyaanController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PertanyaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pertanyaan = DB::table('pertanyaan')->get();
+        //dd($pertanyaan);
+        return view('pertanyaan.index', compact('pertanyaan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pertanyaan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $request->validate([
+            'judul' => 'required|unique:pertanyaan|max:100',
+            'isi' => 'required'
+        ]); 
+        $query = DB::table('pertanyaan')->insert([ 
+            "judul"=>$request["judul"], 
+            "isi" => $request["isi"],
+            "tanggal_dibuat" =>$request["tanggal_dibuat"],
+            "tanggal_diperbarui"=>$request["tanggal_diperbarui"]
+        ]);
+        
+        return redirect('/pertanyaan')->with('success', 'Pertanyaan berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $pertanyaan= DB::table('pertanyaan')->where('id', $id)->first();
+        //dd($pertanyaan);
+        return view ('pertanyaan.show', compact('pertanyaan'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
