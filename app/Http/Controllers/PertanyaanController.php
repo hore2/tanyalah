18d06554692d1471e07db3c9c6932f2be9649de6
f-chilmi <fchilmi5@gmail.com>
2020-08-13 23:29:52 +0700

    controller

---
+++ app/Http/Controllers/PertanyaanController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PertanyaanController extends Controller
+{
+    // index / halaman awal
+    public function index(){
+        $pertanyaan = DB::table('pertanyaan')->get();
+        // dd($pertanyaan);
+        return view('pertanyaan.index', compact('pertanyaan'));
+    }
+
+    // menambah list pertanyaan
+    public function store(Request $request){
+        $pertanyaan = Pertanyaan::create([
+            "judul" => $request["judul"],
+            "isi" => $request["isi"]
+        ]);
+        // dd($pertanyaan);        
+        return redirect ('/pertanyaan')->with('success', 'Pertanyaan berhasil ditambahkan');
+    }
+
+    // menuju halaman create 
+    public function create(){
+        return view('pertanyaan.create');
+    }
+
+    // menampilkan pertanyaan dengan id tertentu
+    public function show($id){
+        $pertanyaan = DB::table('pertanyaan')->where('id', $id)->first();
+        // dd($pertanyaan);
+        return view('pertanyaan.show', compact('pertanyaan'));
+    }
+
+    // update list pertanyaan
+    public function update($id, Request $request){
+        $query = DB::table('pertanyaan')
+                    -> where('id', $id)
+                    -> update([
+                        'judul' => $request['judul'],
+                        'isi' => $request['isi'],
+                    ]);        
+        return redirect ('/pertanyaan')->with('success', 'Pertanyaan berhasil diperbaharui');
+    }
+
+    // hapus list pertanyaan
+    public function destroy($id){
+        $query = DB::table('pertanyaan')->where('id', $id)->delete();
+
+        return redirect ('/pertanyaan')->with('success', 'Berhasil dihapus');
+    }
+
+    public function edit($id){
+        $pertanyaan = DB::table('pertanyaan')->where('id', $id)->first();
+        // dd($pertanyaan);
+
+        return view('pertanyaan.edit', compact('pertanyaan'));
+    }
+}
